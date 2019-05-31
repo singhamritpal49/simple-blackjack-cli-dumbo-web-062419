01eec3a7f70d12aafb462d8d45639f2e09036c80
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-31 15:08:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,7 +33,7 @@ def hit?(card_total)
   prompt_user #puts "Type 'h' to hit or 's' to stay"
 user_input = get_user_input
   if user_input == "h"
-      deal_card
+      card_total += deal_card
   elsif user_input == "s"
     card_total
   else
